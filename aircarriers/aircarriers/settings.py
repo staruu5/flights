1ad3a4c640d383654c64a4e38be84c17a8c0dea2
MacBook Pro <macbookpro@MacBooks-MacBook-Pro.local>
2021-09-29 22:42:24 +0300

    Added django-heroku

--- aircarriers/aircarriers/settings.py
+++ aircarriers/aircarriers/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,3 +155,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100
 }
+
+django_heroku.settings(locals())
